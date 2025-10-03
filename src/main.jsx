a09e4c0c345d439d8fcbcda9070e19791a22b1ff
zehancoder <zehanjh93@gmail.com>
2025-10-03 15:50:48 +0600

    some change

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import UserContextProvider from "./context/UserContextProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <UserContextProvider>
         <App />
-      </UserContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
